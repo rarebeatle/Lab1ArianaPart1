7d8f6330563c635e2021fed1d5d0aa9df33df442
Ari <52987058+rarebeatle@users.noreply.github.com>
2020-09-23 13:36:41 -0600

    Lab

---
+++ MyTestingApp/src/MyTestingApp.java
@@ -0,0 +1,18 @@
+/*
+ * Nombre:Ariana Morales
+ * Lab 1
+ * 
+ */
+
+
+
+public class MyTestingApp {
+   
+  
+    public static void main(String[] args) {
+        
+    MyForm myForm = new MyForm();
+    myForm.setVisible(true);
+    }
+    
+}
